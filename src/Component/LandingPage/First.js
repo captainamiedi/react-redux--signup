1df2f7353e685587a9e8f7019ea763f35162dac3
captainamiedi <captainamiedi1@gmail.com>
2019-10-31 05:59:05 +0100

    heroku build fix

--- src/Component/LandingPage/First.js
+++ src/Component/LandingPage/First.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import phone from '../../images/Phone.png';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import books from '../../images/bookcase-books.jpg';
@@ -13,15 +12,7 @@ function First() {
         <FirstWrapper>
             <Carousel autoPlay interval={1000} infiniteLoop showThumbs={false}  className="carousel">
                 <div className="carousel-container">
-<<<<<<< HEAD
-                    <div>
-                        <p>first carousel</p>
-                        <button>search</button> 
-                    </div>
-                    <img src={phone} alt="phone" style={{width: '25%'}}/>
-=======
                     <img src={books} alt="phone" style={{width: '100%'}}/>
->>>>>>> 0af799ea6b4dfbcebf034accaf821f9a827a308c
                 </div>
                 <div>
                     <img src={clothes} alt="carousel" tyle={{width: '50%'}}/>
